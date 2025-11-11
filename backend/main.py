2f0c3db334f1acd08593ce9c17703b6e22880e23
Krishnakant <krishnakant@visionias.in>
2025-11-11 16:28:57 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -105,7 +105,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://webapp-expense.onrender.com"  
+        "https://webapp-expense.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
