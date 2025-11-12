d79bc76ba1456106a9732fe96cd6fe2681fc17cd
Krishnakant <krishnakant@visionias.in>
2025-11-12 15:47:07 +0530

    meta.env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 // frontend/src/services/api.ts
 
-const API_URL = "http://localhost:8000/api"; // FastAPI backend
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface TokenResponse {
   access_token: string;
